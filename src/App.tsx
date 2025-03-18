4fd8e6cf190b297dc552dcdb6d90ba89e62092bb
consuelogoche-1994 <consuelo.goche@gmail.com>
2025-03-18 18:27:41 -0500

    Setup React + TypeScript + Taiwind css

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-500 text-center'>Vite + React + ts</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
